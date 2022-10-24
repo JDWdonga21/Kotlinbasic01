75b4df67fde568f08226f63de57a63c9261a7760
JDWdonga21 <ongame21212144@gmail.com>
2022-10-24 17:58:11 +0900

    로그 찍어보기

--- app/src/main/java/com/jdwdonga/kotlinbasic01/MainActivity.kt
+++ app/src/main/java/com/jdwdonga/kotlinbasic01/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jdwdonga.kotlinbasic01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        clickBtn
-
-        smallBtn
+        clickBtn.setOnClickListener { 
+            // { } 안에 있는 코드 버튼을 눌렀을 때 실행
+            Log.d("메인화면 로그", "클릭용 버튼 눌림")
+        }
+       
     }
 }
